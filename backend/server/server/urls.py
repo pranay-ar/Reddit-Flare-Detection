5ce66b5be876ed1c4c9352837bdbd4ded9bcdd2e
pranay-ar <a.pranay2001@gmail.com>
2020-04-19 16:23:39 +0530

    endpoints models

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -12,10 +12,22 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.urls import path
+
+from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += endpoints_urlpatterns
